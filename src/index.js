3ca7cf7e97f54231c111bc55b7a1381b48a99c6b
Anastasiia <koicheva1996@gmail.com>
2022-08-21 14:59:01 +0300

    added homework (fastify-routes)

--- src/index.js
+++ src/index.js
@@ -11,4 +11,39 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+const censor = (str, word, reply) =>
+  str.toLowerCase().includes(word)
+    ? reply.status(403).send('unresolved')
+    : reply.send(str);
+
+fastify.post('/uppercase', (request, reply) => {
+  const str = request.body.toUpperCase();
+  censor(str, 'fuck', reply);
+});
+
+fastify.post('/lowercase', (request, reply) => {
+  const str = request.body.toLowerCase();
+  censor(str, 'fuck', reply);
+});
+
+fastify.get('/user/:id', (request, reply) => {
+  const { id } = request.params;
+
+  if (!users[id]) {
+    return reply.status(400).send('User not exist');
+  }
+  reply.send(users[id]);
+});
+
+fastify.get('/users', (request, reply) => {
+  const { filter, value } = request.query;
+
+  if (!filter && !value) {
+    return reply.send(Object.values(users));
+  }
+  return reply.send(
+    Object.values(users).filter((user) => String(user[filter]) === value)
+  );
+});
+
 export default fastify;
